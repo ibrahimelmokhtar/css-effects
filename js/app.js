1e0272116c7e4b5e99d552921c785e2d8fd86bfd
Ibrahim El-Mokhtar <ibrahimelmokhtar@gmail.com>
2022-06-20 23:24:46 +0200

    Modify website's home page

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
-$(document).ready(() => {
-    console.log('Hello World!');
+// Wait until the document is ready
+$(document).ready(function () {
+    // Display number of projects:
+    $('.category h2').each(function () {
+        let counter = $(this).siblings().children().length;
+        $(this).append(`<div class="counter">(${counter})</div>`);
+    });
+
+    // Hide All Projects Except The First One:
+    $('.category:not(:first-of-type) section').slideUp();
+
+    // Handle click events for the titles:
+    $('.category h2').click(function () {
+        // Hide all projects:
+        $('.category section').siblings().not($(this)).siblings().slideUp();
+
+        // Show only the clicked projects:
+        $(this).siblings('section').slideToggle(1000);
+        console.log($(this).siblings('section').children().length);
+    });
 });
